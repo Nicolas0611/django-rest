0fa08366f0b3776630ed6c4546d20dd12d576fd9
Nicolas Sierra <nico.sierra13@gmail.com>
2025-01-03 12:40:13 -0500

    Add order list view and urls endpoint

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
-from api.serializers import ProductSerializer
+from api.serializers import ProductSerializer, OrderSerializer
 from django.shortcuts import get_object_or_404 # type: ignore
 from rest_framework.response import Response # type: ignore
 from rest_framework.decorators import api_view # type: ignore
-from api.models import Product
+from api.models import Product, Order,OrderItem 
 
 @api_view(['GET'])
 def product_list(request):
@@ -14,4 +14,10 @@ def product_list(request):
 def product_detail(request,pk):
     product= get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product) 
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many = True) 
     return Response(serializer.data)
