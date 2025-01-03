03d017782f1d01da4a0510b6be5c4bb8a5062143
Nicolas Sierra <nico.sierra13@gmail.com>
2025-01-02 20:36:11 -0500

    Add product endpoints api view and serializer

--- api/views.py
+++ api/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from api.serializers import ProductSerializer
+from django.shortcuts import get_object_or_404 # type: ignore
+from rest_framework.response import Response # type: ignore
+from rest_framework.decorators import api_view # type: ignore
+from api.models import Product
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many = True) 
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request,pk):
+    product= get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product) 
+    return Response(serializer.data)
