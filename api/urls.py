8973177ed1270231b1a96401bc0b0221cf78a848
Nicolas Sierra <nico.sierra13@gmail.com>
2025-01-05 17:39:37 -0500

    Refactor product list view and detail view

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view()),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),   
     path('products/info/', views.product_info),
-    path('products/<int:pk>/', views.product_detail),   
-    path('orders/', views.order_list),   
+    path('orders/', views.OrderListAPIView.as_view()),   
 ]
