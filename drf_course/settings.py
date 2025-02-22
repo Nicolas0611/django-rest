daf061f9529e16fece80c5b406468614a026bec9
Nicolás Sierra <nico.sierra13@gmail.com>
2025-02-21 23:30:29 -0500

    Add django DRF_SPECTACULAR for frontend docs

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'silk'
+    'silk',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -122,5 +123,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
